615ee6f8d40e96114f035bb4389962c2c30bc778
Alessandro Falcone <alessandro.falcone@ymail.com>
2021-03-07 11:29:19 +0100

    Aggiunto JS e aggiunto styling checkbox

---
+++ js/script.js
@@ -0,0 +1,30 @@
+var adds = 50;
+
+var listIds = ['cheese', 'tomato', 'egg', 'lettuce', 'mustard', 'ketchup'];
+
+
+
+
+document.getElementById('buttonCalculate').addEventListener("click", myFunction);
+
+function myFunction() {
+
+  for (var i = 0; i < listIds.length; i++) {
+    if (document.getElementById(listIds[i]).checked) {
+      adds += 3;
+    }
+  }
+
+  var couponInput = document.getElementById('coupon');
+
+  if (couponInput.value == "ALE2021") {
+    adds = adds * 80 / 100;
+  }
+
+
+  document.getElementById('price').innerHTML = adds;
+  adds = 50;
+}
+
+
+// Prezzo AddsOn: 1: 2: 3: 4: 5: 6:
