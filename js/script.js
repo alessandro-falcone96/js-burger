bd27bd013eb1bad77d3399ea75e76e343ba45d51
Alessandro Falcone <alessandro.falcone@ymail.com>
2021-03-07 11:50:58 +0100

    Aggiunto check per verificare se l'input è vuoto oppure no

--- js/script.js
+++ js/script.js
@@ -8,22 +8,27 @@ var listIds = ['cheese', 'tomato', 'egg', 'lettuce', 'mustard', 'ketchup'];
 document.getElementById('buttonCalculate').addEventListener("click", myFunction);
 
 function myFunction() {
+  var nomeInput = document.getElementById('namePerson');
+  if (nomeInput.value.trim() == '') {
+    alert("Please insert your name");
+  } else {
 
-  for (var i = 0; i < listIds.length; i++) {
-    if (document.getElementById(listIds[i]).checked) {
-      adds += 3;
+    for (var i = 0; i < listIds.length; i++) {
+      if (document.getElementById(listIds[i]).checked) {
+        adds += 3;
+      }
     }
+
+    var couponInput = document.getElementById('coupon');
+    if (couponInput.value == "ALE2021") {
+      adds = adds * 80 / 100;
+    }
+
+    document.getElementById('price').innerHTML = adds;
+    adds = 50;
+
   }
 
-  var couponInput = document.getElementById('coupon');
-
-  if (couponInput.value == "ALE2021") {
-    adds = adds * 80 / 100;
-  }
-
-
-  document.getElementById('price').innerHTML = adds;
-  adds = 50;
 }
 
 
